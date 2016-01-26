9ae8307e10c70277f3ab017af41ec22abf3ef12a
arias01 <Albert_arias01@hotmail.com>
2016-01-26 08:54:53 -0400

    new-Andy

---
+++ github/github/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace github
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("si");
+        }
+    }
+}
